faaf5c15be540d71959edfb4ab8f90a2257474c1
Yiching Hsu <ychsu7149@gmail.com>
2023-06-17 18:26:58 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -9,22 +11,35 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const routes = require("./routes") //預設會去找index.js
+
+const usePassport = require('./config/passport')
+
 require('./config/mongoose')
 const app = express()
-
+const PORT = process.env.PORT
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //true的話, 每次更新都會帶入新的
+    saveUninitialized: true //true會強制把全新的session塞入
+}))
+
+
 // setting static files
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 
 
 // start and listen on the Express server
-app.listen(3000, () => {
-    console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
 })
