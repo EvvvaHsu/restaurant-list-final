87807d540fa823db9a77b9d4f2f0998d51e110d7
Yiching Hsu <ychsu7149@gmail.com>
2023-06-17 22:20:26 +0800

    feat: add isAuthenticated function to views pages

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use((req, res, next) => {
+    console.log(req.user)
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    next()
+})
+
 app.use(routes)
 
 
