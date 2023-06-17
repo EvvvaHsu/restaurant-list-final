31e0d5745f54ee751d0af6f7f876f5a6fa47d9d9
Yiching Hsu <ychsu7149@gmail.com>
2023-06-17 17:19:13 +0800

    feat: add login and register pages with register function and modify css styles

---
+++ routes/modules/users.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const router = express.Router()
+
+const User = require('../../models/user')
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/register', (req, res) => {
+
+    const { name, email, password, confirmPassword } = req.body
+
+    User.findOne({ email })
+        .then(user => {
+            if (user) {
+                console.log('這個email已經註冊過了')
+                return res.render('register', {
+                    name,
+                    email,
+                    password,
+                    confirmPassword
+                })
+            } else {
+                return User.create({
+                    name,
+                    email,
+                    password
+                })
+                    .then(() => {
+                        res.redirect('/')
+                    })
+                    .catch(err => console.log(err))
+            }
+        })
+        .catch(err => console.log(err))
+
+
+
+
+})
+
+
+
+
+module.exports = router
