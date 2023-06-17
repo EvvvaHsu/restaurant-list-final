7fb58f32efaa5406d0ba2be266c611550ee047d4
Yiching Hsu <ychsu7149@gmail.com>
2023-06-17 22:09:25 +0800

    feat: add user authentication function

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const home = require("./modules/home")
 const restaurants = require("./modules/restaurants")
 const search = require("./modules/search")
 const sort = require("./modules/sort")
-const users = require('./modules/users')
+const users = require("./modules/users")
+
+const { authenticator } = require('../middleware/auth')
 
 
-router.use("/restaurants", restaurants)
+router.use("/restaurants", authenticator, restaurants)
 router.use("/search", search)
 router.use("/sort", sort)
-router.use('/users', users)
-router.use("/", home)
+router.use("/users", users)
+router.use("/", authenticator, home)
 
 module.exports = router
