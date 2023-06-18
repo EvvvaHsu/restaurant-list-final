37986af3242a3d26a35285ae4b9a61e68aa34958
Yiching Hsu <ychsu7149@gmail.com>
2023-06-18 14:54:23 +0800

    feat: modify resSeeder to meet one user with three restaurants requirement

--- models/seeds/resSeeder.js
+++ models/seeds/resSeeder.js
@@ -1,5 +1,9 @@
+const bcrypt = require('bcryptjs')
+const { users, restaurants } = require('../seedData')
 const Res = require('../resModel')
-const resList = require('../../restaurant.json')
+const User = require('../user')
+
+// const resList = require('../../restaurant.json')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -8,7 +12,35 @@ if (process.env.NODE_ENV !== 'production') {
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-    Res.create(resList.results)
-    console.log('done')
+
+    Promise.all(
+        users.map((user, index) =>
+            bcrypt.genSalt(10)
+                .then(salt => bcrypt.hash(user.password, salt))
+                .then(hash =>
+                    User.create({
+                        name: user.name,
+                        email: user.email,
+                        password: hash,
+                    })
+                )
+                .then((user) => {
+                    const userId = user._id
+
+                    return Promise.all(
+                        Array.from(
+                            restaurants.slice(index * 3, (index + 1) * 3),
+                            restaurant => Res.create({ ...restaurant, userId })
+                        )
+                    )
+                })
+                .catch(err => console.log(err))
+        )
+    )
+        .then(() => {
+            console.log('done')
+            process.exit()
+        })
+
 })
 
