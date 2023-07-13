f09beaea40fa264344f5df54126822cecd09202e
Yiching Hsu <ychsu7149@gmail.com>
2023-07-14 01:37:46 +0800

    add filter restaurant function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,4 +12,31 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error)) 
 })
 
+router.post('/', (req, res) =>{
+    
+    const userId = req.user._id
+    const order = req.body.alphabeticalOrder
+    // console.log(req.body)
+
+    let sortQuery = {}
+    if (order === 'A-Z') {
+        sortQuery = { name_en: 1 }
+    } else if (order === 'Z-A') {
+        sortQuery = { name_en: -1 }
+    } else if (order === 'sortCategory') {
+        sortQuery = { category: 1 }
+    } else if (order === 'sortLocation') {
+        sortQuery = { location: 1 }
+    }
+
+    return Res
+    .find({ userId })
+    .lean()
+    .sort(sortQuery)
+    .then((restaurants) => 
+        res.render('index', { restaurants, order })
+    )
+    .catch(error => console.error(error))
+})
+
 module.exports = router
